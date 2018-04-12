5192e6dca8bebd1fd1b4f444cf11b97404f6f812
michaelzheng317 <michaelzheng317@gmail.com>
2018-04-12 10:33:46 -0500

    Add .Net Core MVC

---
+++ Michael_Trimble_Challenge_Input/Michael_Trimble_Challenge_Input/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Michael_Trimble_Challenge_Input.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
